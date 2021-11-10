d5ac2c81d970094d8584515a5137d6321d3999b0
ouzm <wjj315315@gmail.com>
2021-11-10 17:08:09 +0800

    lesson 8

--- weather/src/learn_cljs/weather.cljs
+++ weather/src/learn_cljs/weather.cljs
@@ -1,43 +1,20 @@
 (ns ^:figwheel-hooks learn-cljs.weather
   (:require
    [goog.dom :as gdom]
-   [reagent.core :as reagent :refer [atom]]
-   [reagent.dom :as rdom]))
-
-(println "This text is printed from src/learn_cljs/weather.cljs. Go ahead and edit it and see reloading in action.")
-
-(defn multiply [a b] (* a b))
-
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world"}))
-
-(defn get-app-element []
-  (gdom/getElement "app"))
+   [reagent.dom :as rdom]
+   [reagent.core :as r]
+   ))
 
 (defn hello-world []
   [:div
-   [:h1 "I say: " (:text @app-state)]
-   [:h3 "Edit this in src/learn_cljs/weather.cljs and watch it change!"]])
+   [:h1 {:class "app-title"} "Hello, World!"]])
 
-(defn greater []
-  [:div
-   [:h1 "hello ouzm!"]])
-
-(defn mount [el]
-  (rdom/render [hello-world] el))
 
 (defn mount-app-element []
-  (when-let [el (get-app-element)]
-    (mount el)))
-
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
+  (rdom/render [hello-world] (gdom/getElement "app")))
 (mount-app-element)
 
 ;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
   (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
 )
