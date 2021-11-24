286c3401ddd77b6742d3ba5d7addfa55c2996b3f
ou zm <ouzm@ous-MacBook-Pro.local>
2021-11-24 23:55:06 +0800

    chat init

---
+++ code/chat-group/test/learn_cljs/chat_group_test.cljs
@@ -0,0 +1,10 @@
+(ns learn-cljs.chat-group-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]
+     [learn-cljs.chat-group :refer [multiply]]))
+
+(deftest multiply-test
+  (is (= (* 1 2) (multiply 1 2))))
+
+(deftest multiply-test-2
+  (is (= (* 75 10) (multiply 10 75))))
